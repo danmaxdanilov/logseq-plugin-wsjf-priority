ad943e5774bd02e002d1f06437baf0c8d2b1c9df
Maxim Danilov <maximdanilov@example.com>
2025-10-21 20:54:02 +0300

    Prepare for v1.0.3 release

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import logseqDevPlugin from "vite-plugin-logseq";
 
+// Use export default instead of defineConfig directly
 export default defineConfig({
   plugins: [logseqDevPlugin()],
   build: {
